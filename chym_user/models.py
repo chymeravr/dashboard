227470ec8163440c275e1eff93b1e77897d455ef
rubbal <rubbal@chymeravr.com>
2017-01-19 14:09:53 +0530

    Use uuid for userId

--- chym_user/models.py
+++ chym_user/models.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
+import uuid
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -14,6 +16,7 @@ CURRENCY = (
 
 # Create your models here.
 class Profile(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     currency = models.CharField(choices=CURRENCY, default=USD, max_length=20)
     advertising_budget = models.FloatField(default=0)
