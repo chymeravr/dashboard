74c39f87a0f5603cc88747a9743a622c2a096d2a
rubbal <rubbal@chymeravr.com>
2017-02-26 19:06:24 +0530

    Fix profile alignment

--- dashboard/assets/dashboard/js/components/profile.js
+++ dashboard/assets/dashboard/js/components/profile.js
@@ -32,36 +32,35 @@ export class ProfileView extends React.Component {
         var publisherMessage = "Earnings available";
         if (this.state.user && this.state.user.username) {
             var body = (
-                // Key is important for transitions
-                <div key="loaded" className="row">
-                    <Grid centered stretched verticalAlign='middle' columns={4}>
-                        <Grid.Row verticalAlign='middle'>
-                            <Grid.Column verticalAlign='middle'>
-                                <Card>
-                                    <Card.Content>
-                                        <Card.Header>{this.state.user.username}</Card.Header>
-                                        <Card.Description>
-                                            <Statistic>
-                                                <Statistic.Label>Funds</Statistic.Label>
-                                                <Statistic.Value>{this.state.advertising_budget}<Icon name="dollar" size="mini" /></Statistic.Value>
-                                            </Statistic>
-                                            <Statistic>
-                                                <Statistic.Label>Earnings</Statistic.Label>
-                                                <Statistic.Value>{this.state.publisher_earnings}<Icon name="dollar" size="mini" /></Statistic.Value>
-                                            </Statistic>
-                                        </Card.Description>
-                                    </Card.Content>
-                                    <Card.Content extra>
-                                        <div className='ui two buttons'>
-                                            <Button color='blue' as={Link} to='/advertiser/'>Advertise</Button>
-                                            <Button inverted color='blue' as={Link} to='/publisher/'>Monetize</Button>
-                                        </div>
-                                    </Card.Content>
-                                </Card>
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
-                </div>
+                <Grid centered stretched verticalAlign='middle' columns={1}>
+                    <Grid.Row verticalAlign='middle' columns={5}>
+                        <Grid.Column />
+                        <Grid.Column verticalAlign='middle'>
+                            <Card fluid>
+                                <Card.Content>
+                                    <Card.Header>{this.state.user.username}</Card.Header>
+                                    <Card.Description>
+                                        <Statistic>
+                                            <Statistic.Label>Funds</Statistic.Label>
+                                            <Statistic.Value>{this.state.advertising_budget}<Icon name="dollar" size="mini" /></Statistic.Value>
+                                        </Statistic>
+                                        <Statistic>
+                                            <Statistic.Label>Earnings</Statistic.Label>
+                                            <Statistic.Value>{this.state.publisher_earnings}<Icon name="dollar" size="mini" /></Statistic.Value>
+                                        </Statistic>
+                                    </Card.Description>
+                                </Card.Content>
+                                <Card.Content extra>
+                                    <div className='ui two buttons'>
+                                        <Button color='blue' as={Link} to='/advertiser/'>Advertise</Button>
+                                        <Button inverted color='blue' as={Link} to='/publisher/'>Monetize</Button>
+                                    </div>
+                                </Card.Content>
+                            </Card>
+                        </Grid.Column>
+                        <Grid.Column />
+                    </Grid.Row>
+                </Grid>
             )
         } else {
             /**
