419953af004ebe6673a4336b53c59fe37392e85d
rubbal <rubbal@chymeravr.com>
2017-03-03 14:59:30 +0530

    Fix admin registration

--- chym_user/admin.py
+++ chym_user/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 
 from chym_user.models import Profile, InterestedUser
 
-admin.site.register(Profile, InterestedUser)
+admin.site.register(Profile)
+admin.site.register(InterestedUser)
