18d055b05d4d6bb9dd70ce383a5fcb7fe3dcad7f
rubbal <rubbal@chymeravr.com>
2017-03-06 14:18:11 +0530

    Add test device in admin

--- chym_user/admin.py
+++ chym_user/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from chym_user.models import Profile, InterestedUser
+from chym_user.models import Profile, InterestedUser, TestDevice
 
 admin.site.register(Profile)
 admin.site.register(InterestedUser)
+admin.site.register(TestDevice)
