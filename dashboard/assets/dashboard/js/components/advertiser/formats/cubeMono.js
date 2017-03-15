c4fe04a906a70c233955ed67c7effdbb18f1163a
rubbal <rubbal@chymeravr.com>
2017-03-15 23:46:23 +0530

    Add corresponding images for cubemap to equi

--- dashboard/assets/dashboard/js/components/advertiser/formats/cubeMono.js
+++ dashboard/assets/dashboard/js/components/advertiser/formats/cubeMono.js
@@ -39,23 +39,58 @@ export class CubeMonoFormat extends React.Component {
     convertToEqui() {
         var c = document.getElementById("workingCanvas");
         var ctx = c.getContext("2d");
-        // const img = new Image();
-        // img.src = this.state.topImageData;
-        // ctx.drawImage(img, 0, 0, 1024, 1024);
-        // var data = ctx.getImageData(0, 0, 1024, 1024);
+
+        var img = new Image();
+        img.src = this.state.topImageData;
+        ctx.clearRect(0, 0, c.width, c.height);
+
+        ctx.drawImage(img, 0, 0, 1024, 1024);
+        var topImageData = ctx.getImageData(0, 0, 1024, 1024).data;
+
+        img = new Image();
+        img.src = this.state.bottomImageData;
+        ctx.clearRect(0, 0, c.width, c.height);
+        ctx.drawImage(img, 0, 0, 1024, 1024);
+        var bottomImageData = ctx.getImageData(0, 0, 1024, 1024).data;
+
+        img = new Image();
+        img.src = this.state.frontImageData;
+        ctx.clearRect(0, 0, c.width, c.height);
+        ctx.drawImage(img, 0, 0, 1024, 1024);
+        var frontImageData = ctx.getImageData(0, 0, 1024, 1024).data;
+
+        img = new Image();
+        img.src = this.state.backImageData;
+        ctx.clearRect(0, 0, c.width, c.height);
+        ctx.drawImage(img, 0, 0, 1024, 1024);
+        var backImageData = ctx.getImageData(0, 0, 1024, 1024).data;
+
+        img = new Image();
+        img.src = this.state.rightImageData;
+        ctx.clearRect(0, 0, c.width, c.height);
+        ctx.drawImage(img, 0, 0, 1024, 1024);
+        var rightImageData = ctx.getImageData(0, 0, 1024, 1024).data;
+
+        img = new Image();
+        img.src = this.state.leftImageData;
+        ctx.clearRect(0, 0, c.width, c.height);
+        ctx.drawImage(img, 0, 0, 1024, 1024);
+        var leftImageData = ctx.getImageData(0, 0, 1024, 1024).data;
         // console.info(data);
 
         // img.src = this.state.bottomImageData;
         // ctx.drawImage(img, 100, 0, 100, 100);
         // var data = ctx.getImageData(0, 0, 1024, 1024).data;
 
+        const inverseSqrt2 = 0.70710676908493042;
 
 
         var width = 4096,
             height = 2048,
             buffer = new Uint8ClampedArray(width * height * 4);
 
-        var m_theta = 0;
+        var min = 10000
+        var max = 0
         for (var j = 0; j < height; j++) {
             for (var i = 0; i < width; i++) {
                 var x = 2 * i / width;
@@ -65,9 +100,14 @@ export class CubeMonoFormat extends React.Component {
                 var phi = x * Math.PI
                 var theta = y * Math.PI
 
-                var x = Math.cos(phi) * Math.sin(theta)
-                var y = Math.sin(phi) * Math.sin(theta)
-                var z = Math.cos(theta)
+                const cos_phi = Math.cos(phi)
+                const sin_phi = Math.sin(phi)
+                const cos_theta = Math.cos(theta)
+                const sin_theta = Math.sin(theta)
+
+                x = cos_phi * sin_theta
+                y = sin_phi * sin_theta
+                var z = cos_theta
 
                 var fx = Math.abs(x);
                 var fy = Math.abs(y);
@@ -75,50 +115,43 @@ export class CubeMonoFormat extends React.Component {
 
                 const pi = Math.PI;
 
-                var pos = (j * width + i) * 4; // position in buffer based on x and y
-
-                // if (pi / 4 < theta && theta < 3 * pi / 4) {
-                //     if (phi > - pi / 4 && phi < pi / 4) buffer[pos] = 0;
-                //     else if (phi > pi / 4 && phi < 3 * pi / 4) buffer[pos] = 100;
-                //     else if (phi > - 3 * pi / 4 && phi < -pi / 4) buffer[pos] = 200;
-                //     else buffer[pos] = 220;
-                // } else {
-                //     buffer[pos + 1] = 220;
-                // }
-
+                var canvasPosition = (j * width + i) * 4; // position in buffer based on x and y
+                var position = {}
+                var coordinate;
+                var origData;
                 if (fy >= fx && fy >= fz) {
-                    if (y > 0) {
-                        // top face
-                        buffer[pos] = 0;
-                    } else {
-                        // bottom face
-                        buffer[pos] = 0;
-
-                    }
+                    origData = y < 0 ? rightImageData : leftImageData;
+                    position.z = z / fy
+                    position.x = x / fy
+                    position.x = Math.round((1 + position.x) * 512);
+                    position.z = Math.round((1 + position.z) * 512);
+                    min = Math.min(min, position.x)
+                    max = Math.max(max, position.x)
+                    coordinate = (position.x * 1024 + position.z) * 4;
                 } else if (fx >= fy && fx >= fz) {
-                    if (x > 0) {
-                        // right face
-                        buffer[pos + 1] = 200;
-                    } else {
-                        // left face
-                        buffer[pos + 1] = 200;
-                    }
+                    origData = x < 0 ? frontImageData : backImageData;
+                    position.z = z / fx
+                    position.y = y / fx
+                    position.y = Math.round((1 + position.y) * 512);
+                    position.z = Math.round((1 + position.z) * 512);
+                    coordinate = (position.y * 1024 + position.z) * 4;
                 } else {
-                    if (z > 0) {
-                        // front face
-                        buffer[pos] = 100;
-
-                    } else {
-                        // back face
-                        buffer[pos] = 100;
-
-                    }
+                    origData = z > 0 ? topImageData : bottomImageData;
+                    position.x = x / fz
+                    position.y = y / fz
+                    position.x = Math.round((1 + position.x) * 512);
+                    position.y = Math.round((1 + position.y) * 512);
+                    coordinate = (position.y * 1024 + position.x) * 4;
                 }
-                buffer[pos + 3] = 255;           // set alpha channel
+
+                buffer[canvasPosition] = origData[coordinate];
+                buffer[canvasPosition + 1] = origData[coordinate + 1];
+                buffer[canvasPosition + 2] = origData[coordinate + 2];
+                buffer[canvasPosition + 3] = 255;           // set alpha channel
 
             }
         }
-        console.info(theta)
+        console.info(min, max)
         // create imageData object
         var idata = ctx.createImageData(width, height);
 
@@ -130,7 +163,7 @@ export class CubeMonoFormat extends React.Component {
         var previewContext = previewCanvas.getContext("2d");
         var previewImg = new Image();
         previewImg.src = c.toDataURL();
-        previewContext.drawImage(previewImg, 0, 0, 4096, 4096, 0, 0, 300, 300)
+        previewContext.drawImage(previewImg, 0, 0, 4096, 4096, 0, 0, 600, 600)
 
         console.info(this.state)
     }
@@ -156,7 +189,7 @@ export class CubeMonoFormat extends React.Component {
                 </Table>
                 <Button positive content="Convert" onClick={this.convertToEqui} />
                 <canvas id="workingCanvas" height="2048" width="4096" style={{ display: "none" }} />
-                <canvas id="previewCanvas" />
+                <canvas id="previewCanvas" height="600" width="600" />
             </div>
         );
     }
