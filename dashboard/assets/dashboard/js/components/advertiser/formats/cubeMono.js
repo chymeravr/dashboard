dbc8647e6a326a50b18dae6c73747d1310dd4a82
rubbal <rubbal@chymeravr.com>
2017-03-16 16:53:37 +0530

    Fix seams

--- dashboard/assets/dashboard/js/components/advertiser/formats/cubeMono.js
+++ dashboard/assets/dashboard/js/components/advertiser/formats/cubeMono.js
@@ -44,6 +44,7 @@ export class CubeMonoFormat extends React.Component {
     }
 
     convertToEqui() {
+        const imgWidth = 1024;
         var c = document.getElementById("workingCanvas");
         var ctx = c.getContext("2d");
 
@@ -51,59 +52,48 @@ export class CubeMonoFormat extends React.Component {
         img.src = this.state.topImageData;
         ctx.clearRect(0, 0, c.width, c.height);
 
-        ctx.drawImage(img, 0, 0, 1024, 1024);
-        var topImageData = ctx.getImageData(0, 0, 1024, 1024).data;
+        ctx.drawImage(img, 0, 0, imgWidth, imgWidth);
+        var topImageData = ctx.getImageData(0, 0, imgWidth, imgWidth).data;
 
         img = new Image();
         img.src = this.state.bottomImageData;
         ctx.clearRect(0, 0, c.width, c.height);
-        ctx.drawImage(img, 0, 0, 1024, 1024);
-        var bottomImageData = ctx.getImageData(0, 0, 1024, 1024).data;
+        ctx.drawImage(img, 0, 0, imgWidth, imgWidth);
+        var bottomImageData = ctx.getImageData(0, 0, imgWidth, imgWidth).data;
 
         img = new Image();
         img.src = this.state.frontImageData;
         ctx.clearRect(0, 0, c.width, c.height);
-        ctx.drawImage(img, 0, 0, 1024, 1024);
-        var frontImageData = ctx.getImageData(0, 0, 1024, 1024).data;
+        ctx.drawImage(img, 0, 0, imgWidth, imgWidth);
+        var frontImageData = ctx.getImageData(0, 0, imgWidth, imgWidth).data;
 
         img = new Image();
         img.src = this.state.backImageData;
         ctx.clearRect(0, 0, c.width, c.height);
-        ctx.drawImage(img, 0, 0, 1024, 1024);
-        var backImageData = ctx.getImageData(0, 0, 1024, 1024).data;
+        ctx.drawImage(img, 0, 0, imgWidth, imgWidth);
+        var backImageData = ctx.getImageData(0, 0, imgWidth, imgWidth).data;
 
         img = new Image();
         img.src = this.state.rightImageData;
         ctx.clearRect(0, 0, c.width, c.height);
-        ctx.drawImage(img, 0, 0, 1024, 1024);
-        var rightImageData = ctx.getImageData(0, 0, 1024, 1024).data;
+        ctx.drawImage(img, 0, 0, imgWidth, imgWidth);
+        var rightImageData = ctx.getImageData(0, 0, imgWidth, imgWidth).data;
 
         img = new Image();
         img.src = this.state.leftImageData;
         ctx.clearRect(0, 0, c.width, c.height);
-        ctx.drawImage(img, 0, 0, 1024, 1024);
-        var leftImageData = ctx.getImageData(0, 0, 1024, 1024).data;
-        // console.info(data);
-
-        // img.src = this.state.bottomImageData;
-        // ctx.drawImage(img, 100, 0, 100, 100);
-        // var data = ctx.getImageData(0, 0, 1024, 1024).data;
-
-        const inverseSqrt2 = 0.70710676908493042;
-
+        ctx.drawImage(img, 0, 0, imgWidth, imgWidth);
+        var leftImageData = ctx.getImageData(0, 0, imgWidth, imgWidth).data;
 
         var width = 4096,
             height = 2048,
             buffer = new Uint8ClampedArray(width * height * 4);
 
-        var total = 1024 * 1024 * 4;
-        var min = 10000
-        var max = 0
+        var total = imgWidth * imgWidth * 4;
         for (var j = 0; j < height; j++) {
             for (var i = 0; i < width; i++) {
                 var x = 2 * i / width;
                 var y = j / height;
-
 
                 var phi = x * Math.PI
                 var theta = y * Math.PI
@@ -126,35 +116,31 @@ export class CubeMonoFormat extends React.Component {
                 var canvasPosition = (j * width + i) * 4; // position in buffer based on x and y
                 var coordinate;
                 var origData;
-                if (fy >= fx && fy >= fz) {
+                var max = Math.max(fx, fy, fz);
+                if (fy === max) {
                     origData = y < 0 ? rightImageData : leftImageData;
                     z = this.scaleToSize(z / fy);
                     x = this.scaleToSize(x / fy);
-                    coordinate = y < 0 ? total - (z * 1024 - x) * 4 : total - (z * 1024 + x) * 4;
-                } else if (fx >= fy && fx >= fz) {
+                    coordinate = y < 0 ? total - 4 - ((z + 1) * imgWidth - x) * 4 : total - 4 - (z * imgWidth + x) * 4;
+                } else if (fx == max) {
                     origData = x < 0 ? frontImageData : backImageData;
                     z = this.scaleToSize(z / fx);
                     y = this.scaleToSize(y / fx);
-                    coordinate = x < 0 ? total - (z * 1024 + y) * 4 : total - (z * 1024 - y) * 4;
+                    coordinate = x < 0 ? total - 4 - (z * imgWidth + y) * 4 : total - 4 - ((z + 1) * imgWidth - y) * 4;
                 } else {
                     origData = z > 0 ? topImageData : bottomImageData;
                     y = this.scaleToSize(y / fz);
                     x = this.scaleToSize(x / fz);
-                    coordinate = z > 0 ? total - (x * 1024 + y) * 4 : (x * 1024 - y) * 4;
+                    coordinate = z > 0 ? total - 4 - (x * imgWidth + y) * 4 : ((x + 1) * imgWidth - y) * 4;
                 }
 
                 buffer[canvasPosition] = origData[coordinate];
                 buffer[canvasPosition + 1] = origData[coordinate + 1];
                 buffer[canvasPosition + 2] = origData[coordinate + 2];
                 buffer[canvasPosition + 3] = 255;           // set alpha channel
-
             }
         }
-        console.info(min, max)
-        // create imageData object
         var idata = ctx.createImageData(width, height);
-
-        // set our buffer as source
         idata.data.set(buffer);
         ctx.putImageData(idata, 0, 0)
 
@@ -162,9 +148,11 @@ export class CubeMonoFormat extends React.Component {
         var previewContext = previewCanvas.getContext("2d");
         var previewImg = new Image();
         previewImg.src = c.toDataURL();
-        ctx.drawImage(previewImg, 0, 0, 4096, 4096, 0, 0, 4096, 4096)
-        var image = c.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
-        console.info(this.state)
+        previewContext.drawImage(previewImg, 0, 0, 4096, 2048, 0, 0, 600, 300);
+
+        var image = c.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
+        console.info(this.state);
+        window.open(image, "toDataURL() image", "width=600, height=200");
     }
 
     render() {
